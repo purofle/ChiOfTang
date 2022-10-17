56ef1f132bbc68b41a6e3d26619c56e1275b9121
purofle <purofle@gmail.com>
2022-10-17 18:39:52 +0800

    加了个动画包，能动了

--- Assets/Scripts/ChiOfTang/GamePlay/GameCharacterController.cs
+++ Assets/Scripts/ChiOfTang/GamePlay/GameCharacterController.cs
@@ -1,3 +1,4 @@
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 namespace ChiOfTang.GamePlay
@@ -7,19 +8,83 @@ namespace ChiOfTang.GamePlay
         [SerializeField] private CharacterController characterController;
 
         [SerializeField] private bool isRunning;
-        // Start is called before the first frame update
-        void Start()
+        private Animator _animator;
+        private static readonly int IsRunning = Animator.StringToHash("isRunning");
+        private Touch _touch;
+
+        void Awake()
         {
-            
+            _animator = GetComponent<Animator>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            CameraFollow();
+            CheckMobileInput();
             if (isRunning)
             {
-                characterController.Move(Vector3.forward * Time.deltaTime);
+                Running();
+            } else if (_animator.GetBool(IsRunning))
+            {
+                _animator.SetBool(IsRunning, false);
             }
+        }
+
+        private void Running()
+        {
+            characterController.Move(Vector3.forward * Time.deltaTime);
+            if (!_animator.GetBool(IsRunning))
+            {
+                _animator.SetBool(IsRunning, true);
+            }
+        }
+
+        private void CheckMobileInput()
+        {
+            // 手指在屏幕上再跑
+            if (Input.touchCount <= 0) return;
+            _touch = Input.GetTouch(0);
+            isRunning = _touch.phase switch
+            {
+                TouchPhase.Began => true,
+                TouchPhase.Ended => false,
+                _ => isRunning
+            };
+
+            // 滑动移动
+            if (_touch.phase != TouchPhase.Moved) return;
+            var deltaPosition = _touch.deltaPosition;
+            var x = deltaPosition.x;
+            var y = deltaPosition.y;
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                if (x > 0)
+                {
+                    transform.Rotate(Vector3.up, 1);
+                }
+                else
+                {
+                    transform.Rotate(Vector3.up, -1);
+                }
+            }
+            else
+            {
+                if (y > 0)
+                {
+                    transform.Rotate(Vector3.right, 1);
+                }
+                else
+                {
+                    transform.Rotate(Vector3.right, -1);
+                }
+            }
+        }
+
+        void CameraFollow()
+        {
+            // 相机跟随人物
+            
         }
     }
 }
